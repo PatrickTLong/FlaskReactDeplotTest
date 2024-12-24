2004b5871584bc07f0b613bcd4fd1ff07109cdb1
Patrick Long <longpatrick3317@gmail.com>
2024-12-24 10:23:54 -0500

    bu

--- backend/app.py
+++ backend/app.py
@@ -17,12 +17,13 @@ CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))  # Default to 587
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'longpatrick3317@gmail.com'
+app.config['MAIL_PASSWORD'] = 'dqbt ixkt fnvu pesv'  # Be cautious with exposing your password in code
+app.config['MAIL_DEFAULT_SENDER'] = 'longpatrick3317@gmail.com'
+
 
 
 mail = Mail(app)
