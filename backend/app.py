c3257639b403473395feced07968118e6c34efe7
Patrick Long <longpatrick3317@gmail.com>
2024-12-24 10:03:33 -0500

    yessir

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 CORS(app)
 db = SQLAlchemy(app)
