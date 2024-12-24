a81c31e234d48b114398b811df9dead41b9a6402
Patrick Long <longpatrick3317@gmail.com>
2024-12-23 19:31:36 -0500

    YESSSS

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ migrate = Migrate(app,db)
 class Name(db.Model):
     __tablename__ = 'name'
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
-    name : so.Mapped[str] = so.mapped_column(unique=True)
+    name : so.Mapped[str] = so.mapped_column()
 
     def __repr__(self):
         return f"{self.id}/{self.name}"
